561f3ce16cef6afd767dc6f6347c4f4569ebcbfa
Malin Hanak <malinehanak@gmail.com>
2020-03-26 14:40:41 +0100

    added playground

--- index.js
+++ index.js
@@ -65,7 +65,12 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server
   .listen({
