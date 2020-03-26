b34551f4d9507f97ce8d3d21c6ef7ba6dd382144
Malin Hanak <malinehanak@gmail.com>
2020-03-26 14:26:51 +0100

    Added port

--- index.js
+++ index.js
@@ -67,6 +67,10 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`listening at ${url}`);
-});
+server
+  .listen({
+    port: process.env.PORT || 4000,
+  })
+  .then(({ url }) => {
+    console.log(`listening at ${url}`);
+  });
