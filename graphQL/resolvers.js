a3ee3198c19ae7905ae0a05a3637eba22aab68af
Malin Hanak <malinehanak@gmail.com>
2020-04-02 09:46:50 +0200

    Moved to graphQl folder

--- graphQL/resolvers.js
+++ graphQL/resolvers.js
@@ -1,31 +1,14 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { GraphQLScalarType } = require("graphql");
 const { PubSub } = require("apollo-server");
 const { Kind } = require("graphql/language");
 const { Painting, User } = require("./models");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { getUser } = require("../helpers/getUser");
+const { validateUser } = require("../helpers/validateUser");
 
 const pubsub = new PubSub();
 const PAINTING_ADDED = "PAINTING_ADDED";
-
-const getUser = (auth) => {
-  if (auth.error) {
-    if (auth.error.message === "invalid signature") {
-      throw new Error("Your token is incorrect and your are not Authenticated for this action");
-    }
-
-    if (auth.error.name === "TokenExpiredError") throw new Error("Your token has expired");
-  }
-
-  if (!auth.user) throw new Error("Your are not authenticated");
-};
-
-const validateUser = async (user, argsPassword) => {
-  if (!user) throw new Error("Invalid Login");
-
-  const passwordMatch = await bcrypt.compare(argsPassword, user.password);
-  if (!passwordMatch) throw new Error("Invalid Password");
-};
 
 const resolvers = {
   Subscription: {
@@ -35,7 +18,7 @@ const resolvers = {
   },
   Query: {
     currentUser: async (obj, args, { auth }) => {
-      getUser(auth);
+      await getUser(auth);
       const currentUser = await User.findOne({ _id: user.id });
       return currentUser;
     },
@@ -58,27 +41,16 @@ const resolvers = {
     },
     login: async (obj, { username, password }, context, info) => {
       const user = await User.findOne({ username });
-      validateUser(user, password);
+      await validateUser(user, password);
 
-      const token = jwt.sign(
-        {
-          id: user.id,
-          username: user.username
-        },
-        process.env.TOKEN_SUPER_SECRET,
-        {
-          expiresIn: "30d"
-        }
-      );
-      return {
-        token,
-        user
-      };
+      const token = await jwt.sign({ id: user.id, username: user.username }, process.env.TOKEN_SUPER_SECRET, {
+        expiresIn: "30d"
+      });
+      return { token, user };
     },
     addPainting: async (obj, { painting }, { auth }) => {
-      console.log("CONTEXT", auth);
       try {
-        getUser(auth);
+        await getUser(auth);
         pubsub.publish(PAINTING_ADDED, {
           paintingAdded: await Painting.create({
             ...painting
@@ -91,7 +63,7 @@ const resolvers = {
     },
     deleteOnePainting: async (obj, { id }, { auth }) => {
       try {
-        getUser(auth);
+        await getUser(auth);
         return await Painting.deleteOne({ _id: id });
       } catch (error) {
         return error;
@@ -111,7 +83,6 @@ const resolvers = {
     name: "Date",
     description: "Its a date",
     parseValue(value) {
-      // value form client
       return new Date(value);
     },
     serialize(value) {
